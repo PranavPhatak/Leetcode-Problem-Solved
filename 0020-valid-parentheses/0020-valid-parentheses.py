40fd291e25ce684a3af30eb13f44cce6c247157c
Pranav Phatak <110719103+PranavPhatak@users.noreply.github.com>
2026-08-09 23:10:31 +0530

    Time: 0 ms (100%), Space: 19.2 MB (63.31%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -9,16 +9,22 @@ class Solution:
                 if not stack:
                     return False
                 top_element = stack[-1]
-
-                if ((s[i] == ")" and top_element == "(") or 
-                    (s[i] == "]" and top_element == "[") or
-                    (s[i] == "}" and top_element == "{")
-                ):
+                
+                if (self.check_bracs(top_element, s[i])):
                     stack.pop()
                 else:
                     return False
                 
         if not stack:
+            return True
+        else:
+            return False
+
+
+    def check_bracs(self, top_element, open_bracket) -> bool:
+        if ((open_bracket == ")" and top_element == "(") or 
+            (open_bracket == "]" and top_element == "[") or
+            (open_bracket == "}" and top_element == "{")):
             return True
         else:
             return False

e8a95e11a10d70a34b372bdffd19228bb71f8bc2
Pranav Phatak <110719103+PranavPhatak@users.noreply.github.com>
2026-08-11 19:13:44 +0530

    Time: 11 ms (77.42%), Space: 19.4 MB (75.82%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,19 +1,20 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        word1 = {}
-        word2 = {}
-
+        word_count = {}
+        
         for char in s:
-            word1[char] = word1.get(char, 0) + 1
-            if char not in word1:
-                word1[char] = 1
+            word_count[char] = word_count.get(char, 0) + 1
+            if char not in word_count:
+                word[char] = 1
         
         for char in t:
-            word2[char] = word2.get(char, 0) + 1
-            if char not in word2:
-                word2[char] = 1
+            word_count[char] = word_count.get(char, 0) - 1
+            if char not in word_count:
+                word[char] = 1
+        
 
-        if word1 == word2:
+        if all(values == 0 for values in word_count.values()):
             return True
         else:
             return False
+        

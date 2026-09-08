7080daf02b164966a7bfa1a844d7244ef68cb711
Pranav Phatak <110719103+PranavPhatak@users.noreply.github.com>
2026-09-08 18:04:23 +0530

    Time: 9 ms (18.16%), Space: 21.6 MB (10.69%) - LeetHub

---
+++ 0136-single-number/0136-single-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        no_count = {}
+
+        for i in range(len(nums)):
+            if nums[i] not in no_count:
+                no_count[nums[i]] = 1
+            else:
+                no_count[nums[i]] = no_count.get(nums[i], 0) + 1
+        
+        for key, value in no_count.items():
+            if value == 1:
+                return key
